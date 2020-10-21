10cd47bc84ba7cbfc25fe76295866e52f6c0aeb8
Logan Negley <logannegley@gmail.com>
2020-10-21 12:06:45 -0500

    deleted fontawesome installed ant icons

--- src/components/pages/ProfileList/GroomerProfile.js
+++ src/components/pages/ProfileList/GroomerProfile.js
@@ -18,6 +18,8 @@ const GroomerProfile = () => {
 
       <img src="https://via.placeholder.com/720x100"/>
       <h2>Groomer's Business Name</h2>
+      <button>Edit</button>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur sapien a elit iaculis faucibus. Donec molestie, lacus et consequat luctus, odio mi sagittis lacus, vitae blandit nibh libero quis justo. Sed ac pellentesque augue, vitae vestibulum quam. Pellentesque at laoreet ligula. Nunc aliquet lacus urna, vitae eleifend risus venenatis sit amet. Donec pellentesque mollis tortor sed tincidunt. Vestibulum fermentum egestas quam, quis posuere nisl pretium ut. Praesent suscipit porttitor erat. Suspendiss</p>
       <div className='information'>
         <ul>
           <li>999-999-9999</li>
@@ -30,6 +32,7 @@ const GroomerProfile = () => {
           <li>Mon 12-12</li>
           <li>Mon 12-12</li>
         </ul>
+        <button>Go To Messages</button>
       </div>
     </div>
   );
