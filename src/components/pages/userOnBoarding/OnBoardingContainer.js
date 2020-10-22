298f5f575c37c2a2f790b302f7dac0ae3dd57473
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2020-10-22 00:39:27 -0400

    created the initial onboarding form that asks whether a user wants to create a groomer or client profile

--- src/components/pages/userOnBoarding/OnBoardingContainer.js
+++ src/components/pages/userOnBoarding/OnBoardingContainer.js
@@ -1,13 +1,56 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const OnBoardingContainer = () => {
-  const [persona, setPersona] = useState();
+  let history = useHistory();
+
+  const [role, setRole] = useState({
+    role: '',
+  });
+
+  const handleChange = e => {
+    const formData = {
+      ...role,
+      [e.target.name]: e.target.value,
+    };
+    setRole(formData);
+  };
+
+  const onSubmit = () => {
+    window.localStorage.setItem('role', role.role);
+    if (role.role === 'client') {
+      history.push('/onboardingClient');
+    } else {
+      history.push('/onboardingGroomer');
+    }
+  };
+
+  console.log(role);
 
   return (
-    <>
+    <div>
       <h1>Welcome To Express Groomer!</h1>
       <h2>Let's Get Started</h2>
-    </>
+
+      <div>
+        <form onSubmit={onSubmit}>
+          <label>
+            <select
+              placeholder="role"
+              type="text"
+              name="role"
+              value={role.role}
+              onChange={handleChange}
+            >
+              <option value="none">Choose Account Type</option>
+              <option value="groomer">Groomer</option>
+              <option value="client">Client</option>
+            </select>
+          </label>
+        </form>
+        <button onClick={onSubmit}>Continue</button>
+      </div>
+    </div>
   );
 };
 
