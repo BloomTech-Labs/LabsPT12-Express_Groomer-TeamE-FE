9531b8f081e4430929205befe25ed00855ffa354
April Ma <54913304+april5622@users.noreply.github.com>
2020-10-21 23:16:12 -0700

    work in progress for client onboarding form

--- src/components/pages/userOnBoarding/ClientOnBoardingForm.js
+++ src/components/pages/userOnBoarding/ClientOnBoardingForm.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+import axiosWithAuth from '../../../api/axiosWithAuth';
 
-const userOnBoardingForm = () => {
+const clientOnBoardingForm = ({ errors, touched, values, status }) => {
+  // const [data, setData] = useState({
+  //   role: "client",
+  //   bannerUrl: "",
+  //   address: ""
+
+  // })
+
   return (
-    <>
-      <h1>FORM GOES HERE</h1>
-    </>
+    <div>
+      <h1>CLIENT HEY</h1>
+      <div>
+        <Form>
+          <Field
+            type="text"
+            name="address"
+            placeholder="Address"
+            value={values.address}
+          />
+          {touched.address && errors.address && <p>{errors.address}</p>}
+        </Form>
+
+        <button type="submit">Submit</button>
+      </div>
+    </div>
   );
 };
 
-export default userOnBoardingForm;
+const FormikForm = withFormik({
+  mapPropsToValues({ address }) {
+    return {
+      address: address || '',
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    address: Yup.string().required('Required'),
+  }),
+
+  handleSubmit(values, { setStatus, resetForm }) {
+    console.log('hi');
+
+    axiosWithAuth
+      .put()
+      .then(res => {})
+      .catch(err => {});
+  },
+})(clientOnBoardingForm); //FormikForm
+
+export default FormikForm;
